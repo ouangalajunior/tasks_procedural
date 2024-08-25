ee4243d2527cd736416ad7962f6ff12abc7b9998
Junior Ouangala <ouangalajunior@MacBook-Air-de-Junior.local>
2024-08-25 12:08:52 +0200

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+1. Database 
+
+ hostname: "127.0.0.1" or "localhost"
+ username: "php_learning_admin"
+ password: "Geneva2024##"
+ database: "php_learning"
+ port:     3306 or 3307
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//1. Create et connect database
+// Database information
+/*
+ hostname: "127.0.0.1" or "localhost"
+ username: "php_learning_admin"
+ password: "Geneva2024##"
+ database: "php_learning"
+ port:     3306 or 3307
+*/
+//$db = mysqli_connect("localhost","php_learning_admin", "Geneva2024##", "php_learning", 3307);
+?>
