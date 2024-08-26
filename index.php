0eab2737f2dd2047a8f2e2f803b63c3bfc287457
Junior Ouangala <ouangalajunior@MacBook-Air-de-Junior.local>
2024-08-26 12:39:20 +0200

    Display all task order by priority

--- index.php
+++ index.php
@@ -10,39 +10,40 @@
  port:     3306 or 3307
 */
 // Syntax mysqli_connect(host, username, password, dbname, port, socket)
-$db = mysqli_connect("127.0.0.1","root", "", "php_learning", 3308);
+$db = mysqli_connect("127.0.0.1", "root", "", "php_learning", 3308);
 
 // Test if connection succeed
-if(mysqli_connect_errno()){
+if (mysqli_connect_errno()) {
     $msg = "Database connection failed: ";
     $msg .= mysqli_connect_error();
-    $msg .= " (" .mysqli_connect_error() . ")";
+    $msg .= " (" . mysqli_connect_error() . ")";
     exit($msg);
-
-     
-
 }
 // 2. Perform database query to display 1 entry
-     // Data base table name : tasks
+// Data base table name : tasks
 
-     $sql = "SELECT * FROM  tasks LIMIT 1 ";
-     $result = mysqli_query($db, $sql);
+//$sql = "SELECT * FROM  tasks LIMIT 1 ";
+//Display all task order by priority
+$sql = "SELECT * FROM  tasks ORDER BY priority ";
+$result = mysqli_query($db, $sql);
 
-     // Test if query succeed
-     if(!$result){
-        exit("Database query failed");
-     }
-     // 3. Use returned data (if any)
+// Test if query succeed
+if (!$result) {
+    exit("Database query failed");
+}
+// 3. Use returned data (if any)
 $task = mysqli_fetch_assoc($result);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Manager Index page</title>
 </head>
+
 <body>
     <header>
         <h1> Task Managaer</h1>
@@ -50,40 +51,41 @@ $task = mysqli_fetch_assoc($result);
 
 
     <section>
+        <h2>Tasks list</h2>
 
-<h1>Show Task</h1>
-    <!--Display one entry-->
-<dl>
-  <dt>ID</dt>
-  <dd><?php echo $task['id']; ?></dd>
-</dl>
-<dl>
-  <dt>Priority</dt>
-  <dd><?php echo $task['priority']; ?></dd>
-</dl>
-<dl>
-  <dt>Completed</dt>
-  <dd><?php echo $task['complete']  ?></dd>
-</dl>
-<dl>
-  <dt>Description</dt>
-  <dd><?php echo $task['description']; ?></dd>
-</dl>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Priority</th>
+                <th>Completed</th>
+                <th>Description</th>
+                <th>Due Date</th>
+            </tr>
+            <?php
+            // Loop result query all result
+            ?>
+            <?php foreach ($result as $task) { ?>
 
-<dl>
-  <dt>Due Date</dt>
-  <dd><?php echo $task['due_date']; ?></dd>
-</dl>
+                <tr>
+                    <td><?php echo $task['id']; ?> </td>
+                    <td><?php echo $task['priority'] ;?> </td>
+            <!-- Changing boolean 0 and 1 by true or false when task is completed-->
+                    <td><?php echo $task['complete'] == 1  ?  'true' : 'false';?> </td>
+                    <td><?php echo $task['description'] ;?> </td>
+                    <td><?php echo $task['due_date'] ;?> </td>
+                </tr>
+            <?php } ?>
+        </table>
+    </section>
 
-</section>
-
-    <?php 
+    <?php
     // 4. Release returned data
     mysqli_free_result($result);
 
     //5. Close database connection
     mysqli_close($db);
     ?>
-    
+
 </body>
+
 </html>
